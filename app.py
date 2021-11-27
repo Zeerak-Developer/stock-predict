54e92e2fd8e75fd4cfcb45e075236292a48eb259
Zeerak Asif <mzeerak47@gmail.com>
2021-11-27 15:33:38 +0500

    return

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ def home():
 
 @app.route('/predict',methods=['GET'])
 def predict():
-    pkl_file=open('model1.pkl', 'rb')
-    mydict2 = pickle.load(pkl_file)
+    pkl_file=open('model.pkl', 'rb')
+    mydict = pickle.load(pkl_file)
     pkl_file.close( )
 
-    return jsonify({"Result": mydict2})
+    return jsonify({"Result": mydict})
 
 @app.route('/predict1',methods=['GET'])
 def predict1():
-    pkl1_file=open('model2.pkl', 'rb')
-    mydict3=pickle.load(pkl1_file)
+    pkl1_file=open('model1.pkl', 'rb')
+    mydict1=pickle.load(pkl1_file)
     pkl1_file.close( )
 
-    return jsonify({"Result1": mydict3})
+    return jsonify({"Result1": mydict1})
 
 if __name__ == '__main__':
     app.run(debug=True)
